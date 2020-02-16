e67f604e4ac45509a3e4e937ac49ef7b3cc25bf7
VintorezzZ <sergeevmisha16@gmail.com>
2020-02-16 10:34:41 +0300

    add HomeworkModule6

---
+++ HomeworkModule6/HomeworkModule6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeworkModule6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
